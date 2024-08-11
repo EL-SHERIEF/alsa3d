1807c1ffeb41f3f62bb9398a96be1c381c7e7dd4
EL-SHERIEF <ahmed-elsherief@outlook.com>
2024-08-11 20:33:34 +0300

    adding vercel insights

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights/>
   </React.StrictMode>
 );
 
