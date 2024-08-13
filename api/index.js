0877169fe7ec34733218fca3e5697099454449a2
EL-SHERIEF <ahmed-elsherief@outlook.com>
2024-08-13 09:42:23 +0300

    this will work

--- api/index.js
+++ api/index.js
@@ -56,7 +56,7 @@ app.get('/api/getTranslation', (req, res) => {
 
 // API route to save translation
 app.post('/api/saveTranslation', (req, res) => {
-  const filePath = path.resolve(__dirname, '../public/locales/en/translation.json'); // Adjust path as necessary
+  const filePath = path.resolve(__dirname, 'https://abohassan.vercel.app/locales/en/translation.json'); // Adjust path as necessary
   fs.writeFile(filePath, JSON.stringify(req.body, null, 2), 'utf8', (err) => {
     if (err) {
       console.error('Error writing file:', err);
