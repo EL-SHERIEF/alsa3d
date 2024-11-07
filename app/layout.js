4f7adb467a217213699df30f16ef0a770227c216
EL-SHERIEF <ahmed-elsherief@outlook.com>
2024-11-07 07:35:51 +0200

    ar

--- app/layout.js
+++ app/layout.js
@@ -236,7 +236,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" dir="rtl">
+    <html lang="ar" dir="rtl">
       <body className={`${almarai.variable} antialiased`}>
         <Header />
         {children}
