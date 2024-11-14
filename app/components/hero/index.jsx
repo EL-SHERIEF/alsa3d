476c64c4f607aa7077369e6a7e8c98153d76639b
EL-SHERIEF <ahmed-elsherief@outlook.com>
2024-11-14 22:23:40 +0200

    HOTFIX: ItemProp

--- app/components/hero/index.jsx
+++ app/components/hero/index.jsx
@@ -76,13 +76,13 @@ export default function Hero() {
         {slides.map((slide, index) => (
            <div  itemscope itemtype="https://schema.org/Service"  key={index} dir="rtl" className=" h-[26rem] md:h-[calc(100vh-106px)]  w-full !flex flex-col !relative justify-end items-start">
            <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full z-0">
-            <Image src={slide.image} className="left-0 w-full h-full object-cover z-0" fill loading="eager"  placeholder="blur"  quality={75}  itemprop="image" alt="انضمي لنا"/>
+            <Image src={slide.image} className="left-0 w-full h-full object-cover z-0" fill loading="eager"  placeholder="blur"  quality={75}  itemProp="image" alt="انضمي لنا"/>
             </div>
            <div className="mx-1 md:mx-5 w-[90%] md:max-w-4xl ps-5 pb-5 md:ps-10 md:pb-10 z-10 flex flex-col gap-[2px] sm:gap-2 items-start">
-                         <h4 itemprop="name"  className="block text-white z-10 text-[12px] md:text-sm rounded-full border-white border-2 px-4 py-[1px] md:px-6 md:py-1">{slide.tag}</h4>
-                         <h1 itemprop="description"  className="z-10 block text-white text-3xl md:text-5xl/[1.3] font-bold mb-10">{slide.title}</h1>
+                         <h4 itemProp="name"  className="block text-white z-10 text-[12px] md:text-sm rounded-full border-white border-2 px-4 py-[1px] md:px-6 md:py-1">{slide.tag}</h4>
+                         <h1 itemProp="description"  className="z-10 block text-white text-3xl md:text-5xl/[1.3] font-bold mb-10">{slide.title}</h1>
                          <div className="z-10 my-4 flex flex-row gap-[10px]">
-                         <Link itemprop="url" className='rounded-xl bg-custom-gradient py-4 px-10 shadow-sm text-white w-fit flex flex-row gap-1 items-center font-bold' href={slide.linkHref} target="_blank">{slide.linkText}
+                         <Link itemProp="url" className='rounded-xl bg-custom-gradient py-4 px-10 shadow-sm text-white w-fit flex flex-row gap-1 items-center font-bold' href={slide.linkHref} target="_blank">{slide.linkText}
                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.2" stroke="currentColor" className="size-5">
              <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
            </svg>
