7a8ffd3701630ae98cd9ead11132f72a4fd41821
EL-SHERIEF <ahmed-elsherief@outlook.com>
2024-11-19 17:42:34 +0200

    EDIT

--- app/layout.js
+++ app/layout.js
@@ -16,35 +16,7 @@ const almarai = localFont({
   variable: "--font-almarai",
 });
 
-const today = new Date().toISOString();
 
-export const metadata = {
-  title: "ابو حســن | توصيل الكلية التقنية و توصيل الكلية الرقمية",
-  description: "خدمة توصيل الطالبات وتوصيل الموظفات الأعلى أمانًا في المملكة, توصيل طالبات كلية التقنية وتوصيل الكلية الرقمية في الرياض #حضورك_مهمتنا",
-  referrer: 'origin-when-cross-origin',
-  canonical: 'https://abohassan.vercel.app/',
-  siteName: 'ابو حســن',
-  keywords: [
-    'خدمة توصيل طالبات بالرياض', 'توصيل طالبات في حي طويق', 'توصيل طالبات حي العوالي بالرياض',
-    'توصيل طالبات كلية التقنية', 'توصيل طالبات الكلية الرقمية في الرياض', 'توصيل الطالبات غرب الرياض',
-    'توصيل الطالبات في حي الغروب', 'خدمة توصيل طالبات حي نجم الدين', 'توصيل موظفات غرب الرياض',
-    'توصيل موظفات حي الموسى بالرياض', 'توصيل الطالبات والموظفات في الرياض', 'خدمة توصيل في حي طويق بالرياض'
-  ],
-  openGraph: {
-    title: 'ابو حسن | توصيل الكلية التقنية و توصيل الكلية الرقمية',
-    description: "خدمة التوصيل الموسمي الأعلى أمانًا في المملكة#حضورك_مهمتنا",
-    url: 'https://abohassan.vercel.app/',
-    siteName: 'ابو حســن',
-    images: [
-      { url: '/opengraph-image.jpg', width: 1200, height: 630, alt: "خدمة التوصيل الموسمي الأعلى أمانًا" },
-    ],
-    locale: 'ar_SA',
-    type: 'website',
-  },
-  article: {
-    modified_time: today,
-  },
-};
 
 export default function RootLayout({ children }) {
   return (
