db3dcd3af4a7561718ef6addb49bfd0eebed1f88
EL-SHERIEF <ahmed-elsherief@outlook.com>
2024-11-24 08:29:31 +0200

    fix blog slider

--- app/components/blog Slider/index.jsx
+++ app/components/blog Slider/index.jsx
@@ -41,23 +41,25 @@ export default function BlogSlider({ data, title }) {
 
             <Slider {...settings} className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-3 relative">
                 {data.map((post) => (
-                    <section key={post._id} className='relative'>
-                        <div className="h-[400px] md:h-[450px] mx-2 max-w-2xl lg:max-w-4xl rounded-2xl shadow-lg px-4 py-4 sm:py-2 sm:px-2 flex flex-col justify-between  bg-[#ffffffad] backdrop-blur-md border-x-gray-200 border-2 z-10">
+                    <section key={post._id} className='relative group/active'>
+                        <div className=" h-[400px] md:h-[450px] mx-2 max-w-2xl lg:max-w-4xl rounded-2xl shadow-lg px-4 py-4 sm:py-2 sm:px-2 flex flex-col justify-between  bg-[#ffffffad] backdrop-blur-md border-x-gray-200 border-2 z-10">
                             <figure className="text-center">
                                 {/* Main Image */}
                                 {post.mainImage?.asset && (
-                                    <Image
+                                    <div className='overflow-hidden w-full h-[190px] sm:h-[220px]  rounded-lg relative'>
+                                         <Image
                                         src={post.mainImage.asset.url}
                                         alt={post.mainImage.alt || 'Blog Image'}
                                         width={400}
                                         height={200}
-                                        className="rounded-lg sm:rounded-md w-full h-[190px] sm:h-[220px] object-cover"
+                                        className=" w-full h-full sm:rounded-md object-cover group-hover/active:scale-110 duration-200"
                                     />
+                                    </div>
                                 )}
                                 {/* Blog Title */}
                                 
-                                <blockquote className="text-center w-[90%]">
-                                    <h2 className=' text-xl/8 font-semibold text-gray-900 sm:text-1xl/8 mt-3 line-clamp-2'>{post.title}</h2>
+                                <blockquote className="text-center w-[90%] mx-auto">
+                                <Link href={`/blog/${post.slug.current}`} className='group'> <h2 className=' text-xl/8 font-semibold text-gray-900 sm:text-1xl/8 mt-3 line-clamp-2 group-hover:underline'>{post.title}</h2></Link>
                                 </blockquote>
                                 <div className="text-center text-sm text-gray-600 mt-2">
                                         {new Date(post._updatedAt).toLocaleDateString('ar-SA', { dateStyle: 'long' })}
@@ -65,8 +67,8 @@ export default function BlogSlider({ data, title }) {
                                    
                 
                             </figure>
-                            <Link href={`/blog/${post.slug.current}`} passHref className='my-5 mx-7'>
-    <span className="mt-4 inline-block text-blue-600 font-semibold hover:underline">
+                            <Link href={`/blog/${post.slug.current}`} passHref className='my-5 w-fit p-2 mx-7'>
+    <span className=" inline-block text-blue-600 font-semibold hover:underline">
         اقرأ المزيد
     </span>
 </Link>
