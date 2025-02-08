0c29c8e38a85016cead5bc6680edc9c6bf0b0875
EL-SHERIEF <ahmed-elsherief@outlook.com>
2025-02-08 12:54:43 +0200

    s

--- app/components/unavailable.jsx
+++ app/components/unavailable.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 export default function Unavailable() {
   return (
-    <div className='fixed left-0 top-0 w-screen h-screen backdrop:blur-2xl text-2xl text-center text-white bg-black z-50 pointer-events-none'>
-      هذا الموقع غير متوفر
-    </div>
+    <div className='fixed left-0 top-0 w-screen h-screen backdrop:blur-2xl text-2xl text-center text-white bg-black z-50 pointer-events-none flex justify-center items-center'>
+ <h1>      هذا الموقع غير متوفر </h1>  </div>
   )
 }
