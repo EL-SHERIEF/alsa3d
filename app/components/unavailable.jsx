6a87f2ff774ea8f5c21b92933caf351748a044c3
EL-SHERIEF <entkal@outlook.com>
2025-10-12 16:06:13 +0300

    s

--- app/components/unavailable.jsx
+++ app/components/unavailable.jsx
@@ -4,7 +4,7 @@ export default function Unavailable() {
   return (
     <div className='fixed left-0 top-0 w-screen h-screen backdrop:blur-2xl text-2xl text-center text-white bg-black z-50 pointer-events-none flex flex-column justify-center items-center'>
  <h1>الرجاء دفع إلإشتراك السنوي
-  300 USD</h1>  
+  500 USD</h1>  
   <h2>تم إنهاء التعاقد يرجى السنوي يرجى الدفع خلال أيام</h2>
   <p>لا يمكنك استخدام التطبيق حتى يتم دفع الإشتراك السنوي</p>
   <p>Vercel.app</p></div>
